3fe927b42b2e80f9e7991e9a52ae17fb9f9175a9
Samantha Wu <samanthayywu@gmail.com>
2022-07-18 13:13:46 +0100

    all packages

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,7 @@ import helmet from "helmet";
 import { graphglHTTP } from "express-graphql";
 
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(helmet());
