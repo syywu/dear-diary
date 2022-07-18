71a9e94ae2a6e8c755be22284e3a80f7ee656c8b
Samantha Wu <samanthayywu@gmail.com>
2022-07-18 13:23:31 +0100

    schema

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,22 @@
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
-import pkg from "express-graphql";
-const { graphglHTTP } = pkg;
+import { graphqlHTTP } from "express-graphql";
+import schema from "./schema/schema.js";
 
 const app = express();
 const PORT = process.env.PORT;
 
 app.use(cors());
 app.use(helmet());
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  })
+);
 
 app.listen(PORT, () => {
   console.log(`listening to port ${PORT}`);
